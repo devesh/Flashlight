47d82285847b754f0b3fb56f8a222fd3967aaa23
Devesh Parekh <devesh@spamino.com>
2012-12-30 19:44:59 -0800

    Flashlight. Without the pre-Cupcake hacks.

---
+++ src/main/java/org/dyndns/devesh/flashlight/Flashlight.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2012 Devesh Parekh
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.dyndns.devesh.flashlight;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Flashlight extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        // Flashlights should be bright.
+        window.getAttributes().screenBrightness = WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_FULL;
+        window.addFlags(
+                // Use the power button to turn flashlight off and on, even if you have a lock screen.
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                        // We won't do anything with touch events. Don't bother sending them to us.
+                        | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+                        // A flashlight that turns itself off isn't a good flashlight.
+                        | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                        // Turn the screen on if it isn't already on when launching (e.g., from ADB).
+                        | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+    }
+}
